26266d90201dc8205819a15e3b623f8ef1ea3494
unknown <li-tao@hotmail.com>
2017-12-18 22:35:58 +0800

    change keyboard type to input string

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ const dir = require("./config/default");
 
     await page.setViewport({
         width:1920,
-        height:2000,
+        height:1500,
     });
     console.log("setView Page done.");
 
     await page.focus("#kw");
-    await page.keyboard.sendCharacter('壁纸');
+    const search = 'DOTA2 1920*1080';
+    await page.keyboard.type(search,1000);
+    //await page.$eval("#kw",input=>input.setAttribute("value","壁纸"));
     await page.click(".s_search");
     console.log("Find search..");
 
@@ -25,7 +27,7 @@ const dir = require("./config/default");
         const imgSrc = await page.$$eval(".imgitem",imgs => imgs.map(img=>img.getAttribute("data-objurl")));
         console.log(imgSrc.length);
         imgSrc.map(async (img)=>{
-            await page.waitFor(600);
+            await page.waitFor(2000);
             await urlToImage(img,dir.imgDir);
         });
     });
