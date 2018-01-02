2932a4b3b11256bdbdc0383a703796660c05d55c
Lcoder <linelijun@163.com>
2018-01-02 17:15:46 +0800

    add pdf method

--- exp-01.js
+++ exp-01.js
@@ -5,20 +5,26 @@ const dir = require('./config/default');
 (async ()=>{
     const brower = await puppeteer.launch();
     const page = await brower.newPage();
-    await page.goto("https://cn.bing.com/");
+    await page.goto("http://news.baidu.com/");
     console.log("Goto page,loading...");
 
     await page.setViewport({
         width:1920,
-        height:1080,
+        height:3080,
     });
     console.log("Pages viewport setting...");
+    await page.waitFor(6000);
 
-    await page.waitFor(4000);
-
-    const file = path.join(dir.imgDir,`${Date.now()}.png`);
-    await page.screenshot({path:file});
+    const file_png = path.join(dir.imgDir,`${Date.now()}.png`);
+    await page.screenshot({path:file_png, fullscreen:true});
     console.log("screen has been captured.");
+
+    const file_pdf = path.join(dir.imgDir,`${Date.now()}.pdf`);
+    await page.pdf({
+       path:file_pdf,
+       format:"letter"
+    });
+    console.log("Pdf file has been saved.");
 
     await brower.close();
 })();
