10bea615718c113aa7664e860d27e4a3bf081136
Lcoder <linelijun@163.com>
2018-01-03 14:20:30 +0800

    add search method from baiduSearch.

---
+++ exp-search.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+const dir = require('./config/default');
+
+(async ()=>{
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("http://www.baidu.com",{waitUntil:'networkidle2'});
+    console.log("loading...");
+
+    await page.setViewport({
+        width:1920,
+        height:3080
+    });
+    console.log("set Viewport done.");
+
+    const s_def = "盗墓小说";
+    await page.type("#kw",s_def);
+    await page.click("#su");
+    await page.waitFor(2000);  //必须等待，否则浏览器不能正确刷新页面。
+    console.log('Search input has done.');
+
+    const file_pdf = path.join(dir.imgDir,`${Date.now()}.pdf`);
+    const file_png = path.join(dir.imgDir,`${Date.now()}.png`);
+
+    await page.pdf({
+            path:file_pdf,
+            format:"letter"
+    });
+    await page.screenshot({path:file_png,fullscreen:true});
+    console.log("PDF and screenCapture has Process done.");
+
+    await page.waitFor(".c-container");
+
+    const selector = ".c-container h3 a";
+    const text_content = await page.$$eval(selector,anchors=>anchors.map(anchor=>{
+        const title = anchor.innerHTML.trim();
+        const href = anchor.getAttribute("href");
+        return `${title}---${href}`;
+    }));
+    console.log("Find "+`${text_content.length}`+" result.");
+    text_content.map(text=>console.log(text));
+
+   await browser.close();
+})();
